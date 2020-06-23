913cd87e62a105ef57b4d637461b88e0b97ad931
ppd <ppd@iMac-ppd.local>
2020-06-23 23:21:43 +0300

    edited path

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  pathPrefix: `/first-gatsby-site`,
+  pathPrefix: `Gatsby Default Starter`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
